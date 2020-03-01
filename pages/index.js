913c2db19ba6fa983363ea781fa213b68f81dc91
pap7tomek <pap7tomek@gmail.com>
2020-03-01 03:15:00 +0100

    feat: implemented button and header

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,14 @@
+import styles from '../styles.module.css'
+import Link from 'next/link'
 function HomePage() {
-    return <div>Welcome to Next.js!</div>
+    return <div>
+        <div className={styles.header}>klickrent</div>
+        <div className={styles.wrapper}>
+            <Link href="/modal">
+                <a className={styles.button}>Start inquiry</a>
+            </Link>
+        </div>
+    </div>
 }
 
 export default HomePage
